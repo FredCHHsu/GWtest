f8808f0a11a0d4e7e3353c781e7f4cdc11eddd8d
fred.c.h.hsu <fred.c.h.hsu@gmail.com>
2014-07-03 10:29:41 +0800

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GWtest::Application.config.secret_key_base = 'd7ee3e78ddd7b7873d3cff936307817c0ab8317a7f84a8abb5c4c69a7a4912b1a496efcf95133182400c28e0f842319523bb1ceb2918a78706b631d48c9d383d'
